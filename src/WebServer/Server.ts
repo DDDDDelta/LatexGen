e10c276492835aa8ecaf3363ad47fff106e6afe2
LoganZ2 <loganzhuang123@gmail.com>
2025-02-21 17:08:20 -0500

    Decoupled backend server app

---
+++ src/WebServer/Server.ts
@@ -0,0 +1,46 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import gql from 'graphql-tag';
+import DBConnSingle from '../Database';
+
+const storeInfo = async (owner: string, text: string) => {
+    const db =  DBConnSingle.getDatabase();
+    if (db) {
+        const result = await db.collection("info").insertOne({
+            owner: owner,
+            text: text,
+        });
+        return result.acknowledged;
+    }
+    return false;
+};
+
+
+const typeDefs = gql`
+    type Query { 
+        hello: String 
+    } 
+    type Mutation { 
+        storeInfo(owner: String!, text: String!): Boolean! 
+    }
+`;
+const resolvers = { 
+    Query: { 
+        hello: () => "hi" 
+    }, 
+    Mutation: { 
+        storeInfo: storeInfo 
+    } 
+};
+
+async function startServer() {
+
+    const port = 4000;
+
+    let server = new ApolloServer({ typeDefs, resolvers, introspection: true, });
+    const { url } = await startStandaloneServer( server, { listen: { port: port } } );
+    console.log("server started at port " + port);
+    
+}
+
+startServer();
