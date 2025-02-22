9e560b60dec7524c41f4c868e17ac6450894b23f
Steven(Chenwei) Shi <76062592+DDDDDelta@users.noreply.github.com>
2025-02-21 20:28:36 -0500

    remove bad code from Options.ts

--- src/Common/Options.ts
+++ src/Common/Options.ts
@@ -12,30 +12,3 @@ export interface LGOpt {
   readonly Mode: "CLI" | "WS";
 };
 
-function ParseMode(Mode?: string): "CLI" | "WS" | undefined {
-  if (!Mode) {
-    return undefined;
-  }
-
-  if (Mode == "CLI" || Mode == "WS") {
-    return Mode;
-  }
-  else {
-    return undefined;
-  }
-}
-
-const ModeSchema = { 
-  Mode: ParseMode,
-  Port: { type: Number, optional: true },
-  Type: { type: String, optional: true },
-};
-
-const CLISchema = { Type: Number };
-
-const WSSchema = { Port: Number };
-
-export function getLGMode(): "CLI" | "WS" {
-  const res = parse<LGOpt & Partial<LGCLIOpt> & Partial<LGCLIOpt>>(ModeSchema);
-  return res.Mode;
-}
